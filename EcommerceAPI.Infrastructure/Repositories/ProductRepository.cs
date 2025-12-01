89019f63efc90584c8f096b265b870ef1629c9d5
shakana0 <shakanajamal3@gmail.com>
2025-12-01 13:42:01 +0100

    feat: implement product repository

---
+++ EcommerceAPI.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using EcommerceAPI.Domain;
+using EcommerceAPI.Domain.Interfaces;
+using EcommerceAPI.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EcommerceAPI.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly EcommerceApiDbContext _context;
+
+        public ProductRepository(EcommerceApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> AddAsync(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product?> GetByIdAsync(int id)
+        {
+            return await _context.Products
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products
+                .Include(p => p.Category)
+                .ToListAsync();
+        }
+
+        public async Task UpdateAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product is null) return false;
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return true;
+
+        }
+    }
+}
