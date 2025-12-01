c9900c324ccd748f02b393c97c0300caf9b7fd8f
shakana0 <shakanajamal3@gmail.com>
2025-12-01 13:42:10 +0100

    feat: implement category repository

---
+++ EcommerceAPI.Infrastructure/Repositories/CategoryRepository .cs
@@ -0,0 +1,49 @@
+﻿using EcommerceAPI.Domain;
+using EcommerceAPI.Domain.Interfaces;
+using EcommerceAPI.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceAPI.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly EcommerceApiDbContext _context;
+
+        public CategoryRepository(EcommerceApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> AddAsync(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category?> GetByIdAsync(int id)
+        {
+            return await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task UpdateAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null) return false;
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
