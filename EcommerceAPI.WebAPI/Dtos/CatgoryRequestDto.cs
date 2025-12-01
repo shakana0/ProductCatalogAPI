e97345da24b8832f7a7d14973938154167ae5ed2
shakana0 <shakanajamal3@gmail.com>
2025-12-01 13:43:14 +0100

    feat: create category request dto

---
+++ EcommerceAPI.WebAPI/Dtos/CatgoryRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EcommerceAPI.WebAPI.DTOs
+{
+    public class CategoryRequestDto
+    {
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
