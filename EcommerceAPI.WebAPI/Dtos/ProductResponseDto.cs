9419f850b158476849d1dbe4768ecd1b77d10dce
shakana0 <shakanajamal3@gmail.com>
2025-12-01 13:42:36 +0100

    feat: create product response dto

---
+++ EcommerceAPI.WebAPI/Dtos/ProductResponseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace EcommerceAPI.WebAPI.DTOs
+{
+    public class ProductResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+
+        // Relation
+        public int CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+        public string? CategoryDescription { get; set; }
+    }
+}
