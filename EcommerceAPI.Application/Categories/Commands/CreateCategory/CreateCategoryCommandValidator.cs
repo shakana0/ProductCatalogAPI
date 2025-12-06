1d1668660e9836d9ddf1f04398861a789dcd9d0c
shakana0 <shakanajamal3@gmail.com>
2025-12-06 14:31:00 +0100

    feat.: add CreateCategoryCommandValidator

---
+++ EcommerceAPI.Application/Categories/Commands/CreateCategory/CreateCategoryCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace EcommerceAPI.Application.Categories.Commands.CreateCategory
+{
+    public class CreateCategoryCommandValidator: AbstractValidator<CreateCategoryCommand>
+    {
+        public CreateCategoryCommandValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(100).WithMessage("Name must not exceed 100 characters.");
+
+            RuleFor(c => c.Description)
+                .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+        }
+
+    }
+}
