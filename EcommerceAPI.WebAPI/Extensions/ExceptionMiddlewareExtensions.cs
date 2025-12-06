72ca60deb2983f8d0a7ba1560ea43abab189b419
shakana0 <shakanajamal3@gmail.com>
2025-12-06 10:59:53 +0100

    feat. create ExceptionMiddlewareExtensions for fluent validation to show status code 400 instaed of 500

---
+++ EcommerceAPI.WebAPI/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace EcommerceAPI.WebAPI.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseValidationExceptionHandler(this IApplicationBuilder app)
+        {
+            return app.Use(async (context, next) =>
+            {
+                try
+                {
+                    await next();
+                }
+                catch (FluentValidation.ValidationException ex)
+                {
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    context.Response.ContentType = "application/json";
+
+                    var errors = ex.Errors.Select(e => new
+                    {
+                        field = e.PropertyName,
+                        message = e.ErrorMessage
+                    });
+
+                    var response = new
+                    {
+                        title = "Validation Failed",
+                        status = 400,
+                        errors
+                    };
+
+                    await context.Response.WriteAsJsonAsync(response);
+                }
+            });
+        }
+    }
+}
