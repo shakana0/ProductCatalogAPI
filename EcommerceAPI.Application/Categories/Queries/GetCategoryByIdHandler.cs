a081fbae77881fbf551a5f73f54f1772e7b37309
shakana0 <shakanajamal3@gmail.com>
2025-12-06 13:12:08 +0100

    feat: add GetCategoryByIdHandler

---
+++ EcommerceAPI.Application/Categories/Queries/GetCategoryByIdHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using EcommerceAPI.Application.Categories.Dtos;
+using EcommerceAPI.Domain.Interfaces;
+using MediatR;
+
+namespace EcommerceAPI.Application.Categories.Queries
+{
+    public class GetCategoryByIdHandler : IRequestHandler<GetCategoryByIdQuery, CategoryDto?>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public GetCategoryByIdHandler(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CategoryDto?> Handle(GetCategoryByIdQuery query, CancellationToken cancellationToken)
+        {
+            var category = await _categoryRepository.GetByIdAsync(query.Id, cancellationToken);
+
+            if (category == null) return null;
+
+            return _mapper.Map<CategoryDto>(category);
+        }
+    }
+}
