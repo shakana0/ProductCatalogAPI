1a6b1924e70ae1b064ba9fd628dcd0bc2c0e88b9
shakana0 <shakanajamal3@gmail.com>
2025-12-06 14:30:38 +0100

    feat: add CreateCategoryCommand

---
+++ EcommerceAPI.Application/Categories/Commands/CreateCategory/CreateCategoryCommand.cs
@@ -0,0 +1,18 @@
+﻿
+using EcommerceAPI.Application.Categories.Dtos;
+using MediatR;
+
+namespace EcommerceAPI.Application.Categories.Commands.CreateCategory
+{
+    public class CreateCategoryCommand : IRequest<CategoryDto>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public CreateCategoryCommand(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
