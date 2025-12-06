02a04d3bca344a5898e9c3474f4ee1dc787aa758
shakana0 <shakanajamal3@gmail.com>
2025-12-06 13:57:06 +0100

    refactor: add CancellationToken

--- EcommerceAPI.Domain/Interfaces/IProductRepository.cs
+++ EcommerceAPI.Domain/Interfaces/IProductRepository.cs
@@ -1,15 +1,17 @@
 ﻿
+using EcommerceAPI.Domain.Entities;
+
 namespace EcommerceAPI.Domain.Interfaces
 {
     public interface IProductRepository
     {
-        Task<Product> AddAsync(Product product);
+        Task<Product> AddAsync(Product product, CancellationToken cancellationToken);
 
-        Task<Product?> GetByIdAsync(int id);
-        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product?> GetByIdAsync(int id, CancellationToken cancellationToken);
+        Task<IEnumerable<Product>> GetAllAsync(CancellationToken cancellationToken);
 
-        Task UpdateAsync(Product product);
+        Task UpdateAsync(Product product, CancellationToken cancellationToken);
 
-        Task<bool> DeleteAsync(int id);
+        Task<bool> DeleteAsync(int id, CancellationToken cancellationToken);
     }
 }
