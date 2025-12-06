1e94dbdfe3c6b20976c3cecd466e4169262b4237
shakana0 <shakanajamal3@gmail.com>
2025-12-06 16:24:17 +0100

    refactor: update rule to only validate when certain fields are given

--- EcommerceAPI.Application/Categories/Commands/UpdateCatgory/UpdateCategoryCommandValidator.cs
+++ EcommerceAPI.Application/Categories/Commands/UpdateCatgory/UpdateCategoryCommandValidator.cs
@@ -6,9 +6,16 @@ namespace EcommerceAPI.Application.Categories.Commands.UpdateCategory
     {
         public UpdateCategoryCommandValidator()
         {
-            RuleFor(c => c.Id).GreaterThan(0);
-            RuleFor(c => c.Name).NotEmpty().MaximumLength(100);
-            RuleFor(c => c.Description).MaximumLength(500);
+            RuleFor(c => c.Id)
+                .GreaterThan(0).WithMessage("Category Id must be greater than zero.");
+
+            RuleFor(c => c.Name)
+                .MaximumLength(100).WithMessage("Name must not exceed 100 characters.")
+                .When(c => !string.IsNullOrWhiteSpace(c.Name));
+
+            RuleFor(c => c.Description)
+                .MaximumLength(500).WithMessage("Description must not exceed 500 characters.")
+                .When(c => !string.IsNullOrWhiteSpace(c.Description));
         }
     }
 }
