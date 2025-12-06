78ece99ba002dcae5d5dd45f3adf33920c32d44c
shakana0 <shakanajamal3@gmail.com>
2025-12-06 13:23:27 +0100

    refactor: add cancellationToken to methods

--- EcommerceAPI.Infrastructure/Repositories/CategoryRepository .cs
+++ EcommerceAPI.Infrastructure/Repositories/CategoryRepository .cs
@@ -1,4 +1,4 @@
-﻿using EcommerceAPI.Domain;
+﻿using EcommerceAPI.Domain.Entities;
 using EcommerceAPI.Domain.Interfaces;
 using EcommerceAPI.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
@@ -14,35 +14,38 @@ namespace EcommerceAPI.Infrastructure.Repositories
             _context = context;
         }
 
-        public async Task<IEnumerable<Category>> GetAllAsync()
+        public async Task<IEnumerable<Category>> GetAllAsync(CancellationToken cancellationToken)
         {
-            return await _context.Categories.ToListAsync();
+            return await _context.Categories.ToListAsync(cancellationToken);
         }
 
-        public async Task<Category> AddAsync(Category category)
+        public async Task<Category> AddAsync(Category category, CancellationToken cancellationToken)
         {
             _context.Categories.Add(category);
-            await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync(cancellationToken);
             return category;
         }
 
-        public async Task<Category?> GetByIdAsync(int id)
+        public async Task<Category?> GetByIdAsync(int id, CancellationToken cancellationToken)
         {
-            return await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            return await _context.Categories.FirstOrDefaultAsync(c => c.Id == id, cancellationToken);
         }
 
-        public async Task UpdateAsync(Category category)
+        public async Task UpdateAsync(Category category, CancellationToken cancellationToken)
         {
             _context.Categories.Update(category);
-            await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync(cancellationToken);
         }
 
-        public async Task<bool> DeleteAsync(int id)
+        public async Task<bool> DeleteAsync(int id, CancellationToken cancellationToken)
         {
-            var category = await _context.Categories.FindAsync(id);
+            var category = await GetByIdAsync(id, cancellationToken);
+
             if (category == null) return false;
             _context.Categories.Remove(category);
-            await _context.SaveChangesAsync();
+
+            await _context.SaveChangesAsync(cancellationToken);
+
             return true;
         }
     }
