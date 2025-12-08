e3dae0e3d4fe010fc54ed66fdc03d74a3aa6f288
shakana0 <shakanajamal3@gmail.com>
2025-12-08 11:28:16 +0100

    make swagger avalible in prod too

--- EcommerceAPI.WebAPI/Program.cs
+++ EcommerceAPI.WebAPI/Program.cs
@@ -60,11 +60,8 @@ var app = builder.Build();
 
 app.UseCors("AllowFrontend");
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseValidationExceptionHandler();
 app.UseAuthorization();
