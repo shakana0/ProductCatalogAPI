3bba505bd5aa88fa13563dda5ff78ca9413b9a0a
shakana0 <shakanajamal3@gmail.com>
2025-12-10 14:19:42 +0100

    feat: create SecretsConfig class

---
+++ EcommerceAPI.Application/Infrastructure/Config/LicenseConfig.cs
@@ -0,0 +1,8 @@
+namespace EcommerceAPI.Application.Infrastructure.Config
+{
+    public class SecretsConfig
+    {
+        public string LicenseKey { get; set; } = string.Empty;
+        public string ConnectionString { get; set; } = string.Empty;
+    }
+}
