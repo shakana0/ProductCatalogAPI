dc78eaf0a278295bc96ad401c188ca716499ca2f
shakana0 <shakanajamal3@gmail.com>
2025-12-13 11:51:32 +0100

    feat: inherit PagedResult in GetAllProductsQuery

--- Optimized/EcommerceAPI.Application/Products/Queries/GetAllProducts/GetAllProductsQuery.cs
+++ Optimized/EcommerceAPI.Application/Products/Queries/GetAllProducts/GetAllProductsQuery.cs
@@ -3,7 +3,9 @@ using EcommerceAPI.Application.Products.Dtos;
 
 namespace EcommerceAPI.Application.Products.Queries.GetProducts
 {
-    public class GetAllProductsQuery : IRequest<IEnumerable<ProductDto>>
+    public class GetAllProductsQuery : IRequest<PagedResult<ProductDto>>
     {
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
     }
 }
